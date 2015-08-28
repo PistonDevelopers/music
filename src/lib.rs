6ded601db205f185e0759301bcda1ef0e52f12a3
Sven Nilsen <bvssvni@gmail.com>
2015-08-28 20:03:30 +0200

    Upgrade to latest Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-
+#![feature(reflect_marker)]
 #![deny(missing_docs)]
 
 //! A high level library for playing music
@@ -11,9 +11,9 @@ use sdl2_mixer as mix;
 use current::{ Current, CurrentGuard };
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::marker::Reflect;
 
 fn init_audio() {
-    sdl2::init(sdl2::INIT_AUDIO | sdl2::INIT_TIMER);
     // Load dynamic libraries.
     mix::init(
           mix::INIT_MP3
@@ -32,12 +32,16 @@ fn init_audio() {
     mix::allocate_channels(mix::DEFAULT_CHANNELS);
 }
 
-unsafe fn current_music_tracks<T: 'static>() -> Current<HashMap<T, mix::Music>> {
+unsafe fn current_music_tracks<T: 'static + Reflect>() -> Current<HashMap<T, mix::Music>> {
     Current::new()
 }
 
 /// Initializes audio and sets up current objects.
-pub fn start<T: Eq + Hash + 'static>(f: ||) {
+pub fn start<T: Eq + Hash + 'static + Reflect, F: FnOnce()>(f: F) {
+    let sdl = sdl2::init().unwrap();
+    let audio = sdl.audio().unwrap();
+    let timer = sdl.timer().unwrap();
+    
     init_audio();
     let mut music_tracks: HashMap<T, mix::Music> = HashMap::new();
 
@@ -46,16 +50,19 @@ pub fn start<T: Eq + Hash + 'static>(f: ||) {
     f();
 
     drop(music_tracks_guard);
+    drop(timer);
+    drop(audio);
+    drop(sdl);
 }
 
 /// Binds a file to value.
-pub fn bind_file<T: Eq + Hash + 'static>(val: T, file: &str) {
-    let track = mix::Music::from_file(&Path::new(file)).unwrap();
+pub fn bind_file<T: Eq + Hash + 'static + Reflect>(val: T, file: &str) {
+    let track = mix::Music::from_file(&file.as_ref()).unwrap();
     unsafe { current_music_tracks() }.insert(val, track);
 }
 
 /// Tells how many times to repeat.
-#[deriving(Copy)]
+#[derive(Copy, Clone)]
 pub enum Repeat {
     /// Repeats forever.
     Forever,
@@ -64,18 +71,18 @@ pub enum Repeat {
 }
 
 impl Repeat {
-    fn to_sdl2_repeats(&self) -> int {
+    fn to_sdl2_repeats(&self) -> isize {
         match self {
             &Repeat::Forever => -1,
             &Repeat::Times(val) => {
-                val as int
+                val as isize
             }
         }
     }
 }
 
 /// Plays a music track.
-pub fn play<T: Eq + Hash + 'static>(val: &T, repeat: Repeat) {
+pub fn play<T: Eq + Hash + 'static + Reflect>(val: &T, repeat: Repeat) {
     let _ = unsafe { current_music_tracks::<T>() }.get(val)
         .expect("music: Attempted to play value that is not bound to asset")
         .play(repeat.to_sdl2_repeats());
