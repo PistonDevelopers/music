2bd3146ba7683621e4b66b23319d5f72815e73e6
johnthagen <johnthagen@gmail.com>
2017-01-24 17:34:04 -0500

    Run rustfmt.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ extern crate current;
 extern crate sdl2;
 
 use sdl2::mixer as mix;
-use current::{ Current, CurrentGuard };
+use current::{Current, CurrentGuard};
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::any::Any;
@@ -21,20 +21,13 @@ pub const MAX_VOLUME: f64 = 1.0;
 fn init_audio() {
     // Load dynamic libraries.
     // Ignore formats that are not built in.
-    let _ = mix::init(
-          mix::INIT_MP3
-        | mix::INIT_FLAC
-        | mix::INIT_MOD
-        | mix::INIT_FLUIDSYNTH
-        | mix::INIT_MODPLUG
-        | mix::INIT_OGG
-    );
-    mix::open_audio(
-        mix::DEFAULT_FREQUENCY,
-        mix::DEFAULT_FORMAT,
-        mix::DEFAULT_CHANNELS,
-        1024
-    ).unwrap();
+    let _ = mix::init(mix::INIT_MP3 | mix::INIT_FLAC | mix::INIT_MOD | mix::INIT_FLUIDSYNTH |
+                      mix::INIT_MODPLUG | mix::INIT_OGG);
+    mix::open_audio(mix::DEFAULT_FREQUENCY,
+                    mix::DEFAULT_FORMAT,
+                    mix::DEFAULT_CHANNELS,
+                    1024)
+        .unwrap();
     mix::allocate_channels(mix::DEFAULT_CHANNELS);
 }
 
@@ -83,9 +76,7 @@ impl Repeat {
     fn to_sdl2_repeats(&self) -> i32 {
         match *self {
             Repeat::Forever => -1,
-            Repeat::Times(val) => {
-                val as i32
-            }
+            Repeat::Times(val) => val as i32,
         }
     }
 }
@@ -97,14 +88,14 @@ impl Repeat {
 /// Values less than 0.0 will use 0.0.
 pub fn set_volume(volume: f64) {
     // Map 0.0 - 1.0 to 0 - 128 (sdl2_mixer::MAX_VOLUME).
-    mix::Music::set_volume((volume.max(MIN_VOLUME).min(MAX_VOLUME) * mix::MAX_VOLUME as f64)
-        as i32);
+    mix::Music::set_volume((volume.max(MIN_VOLUME).min(MAX_VOLUME) * mix::MAX_VOLUME as f64) as
+                           i32);
 }
 
 /// Plays a music track.
 pub fn play<T: Eq + Hash + 'static + Any>(val: &T, repeat: Repeat) {
-    let _ = unsafe { current_music_tracks::<T>() }.get(val)
+    let _ = unsafe { current_music_tracks::<T>() }
+        .get(val)
         .expect("music: Attempted to play value that is not bound to asset")
         .play(repeat.to_sdl2_repeats());
 }
-
