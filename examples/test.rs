63df263cb5022f2539b689bce07bc4e550995ecc
johnthagen <johnthagen@gmail.com>
2017-05-27 17:05:09 -0400

    Add support for sound effects.

--- examples/test.rs
+++ examples/test.rs
@@ -8,6 +8,11 @@ enum Music {
     Piano,
 }
 
+#[derive(Copy, Clone, Hash, PartialEq, Eq)]
+enum Sound {
+    Ding,
+}
+
 fn main() {
     let mut window: PistonWindow = WindowSettings::new("Test music", [640, 480])
         .exit_on_esc(true)
@@ -15,9 +20,12 @@ fn main() {
         .unwrap();
 
     music::start::<Music, _>(|| {
-        music::bind_file(Music::Piano, "./assets/piano.wav");
-        music::set_volume(music::MAX_VOLUME);
-        music::play(&Music::Piano, music::Repeat::Forever);
+        music::bind_music_file(Music::Piano, "./assets/piano.wav");
+        music::bind_sound_file(Sound::Ding, "./assets/ding.wav");
+
+        music::set_music_volume(music::MAX_VOLUME);
+        music::play_music(&Music::Piano, music::Repeat::Forever);
+        music::play_sound(&Sound::Ding, music::Repeat::Times(2));
 
         while let Some(e) = window.next() {
             window.draw_2d(&e, |_c, g| { clear([1.0; 4], g); });
--- src/lib.rs
+++ src/lib.rs
@@ -29,10 +29,16 @@ fn init_audio() {
                       mixer::DEFAULT_CHANNELS,
                       1024)
             .unwrap();
-    mixer::allocate_channels(mixer::DEFAULT_CHANNELS);
+    // Sets the number of simultaneous sound effects channels
+    // that are available.
+    mixer::allocate_channels(16);
 }
 
 unsafe fn current_music_tracks<T: 'static + Any>() -> Current<HashMap<T, mixer::Music<'static>>> {
+    Current::new()
+}
+
+unsafe fn current_sound_tracks<T: 'static + Any>() -> Current<HashMap<T, mixer::Chunk>> {
     Current::new()
 }
 
@@ -44,24 +50,36 @@ pub fn start<T: Eq + Hash + 'static + Any, F: FnOnce()>(f: F) {
 
     init_audio();
     let mut music_tracks: HashMap<T, mixer::Music> = HashMap::new();
-
     let music_tracks_guard = CurrentGuard::new(&mut music_tracks);
+
+    let mut sound_tracks: HashMap<T, mixer::Chunk> = HashMap::new();
+    let sound_tracks_guard = CurrentGuard::new(&mut sound_tracks);
 
     f();
 
+    drop(sound_tracks_guard);
     drop(music_tracks_guard);
     drop(timer);
     drop(audio);
     drop(sdl);
 }
 
-/// Binds a file to value.
-pub fn bind_file<T, P>(val: T, file: P)
+/// Binds a music file to value.
+pub fn bind_music_file<T, P>(val: T, file: P)
     where T: 'static + Eq + Hash + Any,
           P: AsRef<Path>
 {
     let track = mixer::Music::from_file(file.as_ref()).unwrap();
     unsafe { current_music_tracks() }.insert(val, track);
+}
+
+/// Binds a sound file to value.
+pub fn bind_sound_file<T, P>(val: T, file: P)
+    where T: 'static + Eq + Hash + Any,
+          P: AsRef<Path>
+{
+    let track = mixer::Chunk::from_file(file.as_ref()).unwrap();
+    unsafe { current_sound_tracks() }.insert(val, track);
 }
 
 /// Tells how many times to repeat.
@@ -87,16 +105,26 @@ impl Repeat {
 /// The volume is set on a scale of 0.0 to 1.0, which means 0-100%.
 /// Values greater than 1.0 will use 1.0.
 /// Values less than 0.0 will use 0.0.
-pub fn set_volume(volume: f64) {
+pub fn set_music_volume(volume: f64) {
     // Map 0.0 - 1.0 to 0 - 128 (sdl2::mixer::MAX_VOLUME).
     mixer::Music::set_volume((volume.max(MIN_VOLUME).min(MAX_VOLUME) *
                               mixer::MAX_VOLUME as f64) as i32);
 }
 
 /// Plays a music track.
-pub fn play<T: Eq + Hash + 'static + Any>(val: &T, repeat: Repeat) {
+pub fn play_music<T: Eq + Hash + 'static + Any>(val: &T, repeat: Repeat) {
     let _ = unsafe { current_music_tracks::<T>() }
         .get(val)
         .expect("music: Attempted to play value that is not bound to asset")
         .play(repeat.to_sdl2_repeats());
+}
+
+/// Plays a sound effect track.
+pub fn play_sound<T: Eq + Hash + 'static + Any>(val: &T, repeat: Repeat) {
+    unsafe {
+        sdl2::mixer::Channel::all().play(current_sound_tracks::<T>()
+            .get(val)
+            .expect("music: Attempted to play value that is not bound to asset"),
+                                         repeat.to_sdl2_repeats());
+    }
 }
