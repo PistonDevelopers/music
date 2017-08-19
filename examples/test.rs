b476d7f6615aad01e3c1d81898ab2bae40753c4f
johnthagen <johnthagen@users.noreply.github.com>
2017-08-19 03:30:56 -0400

    Allow user to change sound volume independent of music volume (#66)

--- examples/test.rs
+++ examples/test.rs
@@ -25,7 +25,7 @@ fn main() {
 
         music::set_volume(music::MAX_VOLUME);
         music::play_music(&Music::Piano, music::Repeat::Forever);
-        music::play_sound(&Sound::Ding, music::Repeat::Times(1));
+        music::play_sound(&Sound::Ding, music::Repeat::Times(1), music::MAX_VOLUME);
         while let Some(e) = window.next() {
             window.draw_2d(&e, |_c, g| { clear([1.0; 4], g); });
         }
